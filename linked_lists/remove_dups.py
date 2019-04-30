eaa8fcec14cffbe1d733ff2a484a4594ee7ffdf3
Connor Oswold <coswoldo@gmail.com>
2019-04-29 20:04:55 -0700

    refactored code for better time complexity

--- linked_lists/remove_dups.py
+++ linked_lists/remove_dups.py
@@ -1,17 +1,20 @@
 """Q2.1"""
 from linked_list import LinkedList
 
+"""O(n) time complexity"""
 def remove_dups(linked_list):
     buffer = {}
     current_node = linked_list.head
+    previous = None
     while current_node is not None:
         if current_node.data not in buffer:
             buffer[current_node.data] = 1
+            previous = current_node
         else:
-            linked_list.delete(current_node.data)
+            previous.next = current_node.next
         current_node = current_node.next
     return linked_list.items()
 
 if __name__ == '__main__':
-    ll = LinkedList(['a', 'c', 'c', 'c', 'b', 'b', 'b', 'c', 'c'])
+    ll = LinkedList(['a', 'b', 'c', 'c', 'c', 'b', 'b', 'b', 'c', 'c'])
     print(remove_dups(ll))
