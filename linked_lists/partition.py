423f03a5104f72cb2a716f2374d586b77bab9a38
Connor Oswold <coswoldo@gmail.com>
2019-05-07 23:19:58 -0700

    optimized solution

--- linked_lists/partition.py
+++ linked_lists/partition.py
@@ -14,8 +14,21 @@ def partition (linked_list, part):
                     break
                 runner = runner.next
         node = node.next
-    return linked_list        
+    return linked_list
+
+def optimized_partition (linked_list, part):
+    new_ll = LinkedList()
+    search = linked_list.head
+    while search is not None:
+        if search.data < part:
+            new_ll.prepend(search.data)
+        else:
+            new_ll.append(search.data)
+        search = search.next
+    return new_ll
 
 if __name__ == '__main__':
     ll = LinkedList([3, 5, 8, 5, 10, 2, 1])
-    print(partition(ll, 5))
+    part = 2
+    print(partition(ll, part))
+    print(optimized_partition(ll, part))
