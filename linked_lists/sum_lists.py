a415909c7106d57373dd6ffbe70691ffd6b6128b
Connor Oswold <coswoldo@gmail.com>
2019-05-13 23:22:28 -0700

    solving Q2.4

---
+++ linked_lists/sum_lists.py
@@ -0,0 +1,11 @@
+"""Q2.4"""
+from linked_list import LinkedList
+
+def sum_lists(ll1, ll2):
+    current_node = ll1.head()
+    current_node2 = ll2.head()
+
+if __name__ == '__main__':
+    ll1 = LinkedList([5, 6, 7])
+    ll2 = LinkedList([4, 5, 6])
+    print(sum_list(ll1, ll2))
