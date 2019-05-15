55fef0e39d5e29ebac71c9a1e4ef2d4382fb83fd
Connor Oswold <coswoldo@gmail.com>
2019-05-14 23:09:11 -0700

    Q2.4

--- linked_lists/sum_lists.py
+++ linked_lists/sum_lists.py
@@ -1,11 +1,29 @@
 """Q2.4"""
-from linked_list import LinkedList
+from linked_list import LinkedList, Node
 
-def sum_lists(ll1, ll2):
-    current_node = ll1.head()
-    current_node2 = ll2.head()
+def sum_lists(ll1, ll2, carry=0):
+    if ll1 == None and ll2 == None and carry == 0:
+        return None
+    value = carry
+    if ll1 != None:
+        value += ll1.data
+    if ll2 != None:
+        value += ll2.data
+    result = Node(value % 10)
+    if ll1 != None or ll2 != None:
+        if value >= 10:
+            value = 1
+        else:
+            value = 0
+        more = sum_lists(ll1.next, ll2.next, value)
+        result.next = more
+    return result
+
 
 if __name__ == '__main__':
     ll1 = LinkedList([5, 6, 7])
     ll2 = LinkedList([4, 5, 6])
-    print(sum_list(ll1, ll2))
+    result = sum_lists(ll1.head, ll2.head)
+    while result != None:
+        print(result.data)
+        result = result.next
