55d3a65f14d91a8f3dbbdd1700d3282678ed8906
Connor Oswold <coswoldo@gmail.com>
2019-05-14 23:17:39 -0700

    fixed broken code for different length lists

--- linked_lists/sum_lists.py
+++ linked_lists/sum_lists.py
@@ -15,14 +15,19 @@ def sum_lists(ll1, ll2, carry=0):
             value = 1
         else:
             value = 0
-        more = sum_lists(ll1.next, ll2.next, value)
+        if ll1 == None:
+            more = sum_lists(None, ll2.next, value)
+        elif ll2 == None:
+            more = sum_lists(ll1.next, None, value)
+        else:
+            more = sum_lists(ll1.next, ll2.next, value)
         result.next = more
     return result
 
 
 if __name__ == '__main__':
-    ll1 = LinkedList([5, 6, 7])
-    ll2 = LinkedList([4, 5, 6])
+    ll1 = LinkedList([0, 5])
+    ll2 = LinkedList([4])
     result = sum_lists(ll1.head, ll2.head)
     while result != None:
         print(result.data)
