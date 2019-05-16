0e879484fe9fd1f35f09c818e8b55e60104d335f
Connor Oswold <coswoldo@gmail.com>
2019-05-15 21:09:05 -0700

    refactored solution with help

--- linked_lists/palindrome.py
+++ linked_lists/palindrome.py
@@ -1,6 +1,7 @@
 """Q2.6"""
 from linked_list import LinkedList
 
+"""O(2n) == O(n) Time Complexity"""
 def is_palindrome(node):
     # Find length
     runner = node
@@ -15,7 +16,6 @@ def is_palindrome(node):
         if length > half:
             stack.append(node.data)
         elif stack[-1] == node.data:
-            print(stack[-1])
             stack.pop()
         node = node.next
         length -= 1
@@ -24,8 +24,28 @@ def is_palindrome(node):
         return True
     return False
 
+"""Refactored Solution"""
+def is_palindrome_o(node):
+    fast = node
+    stack = []
+    # Find the midpoint and push data onto stack
+    while fast != None and fast.next != None:
+        stack.append(node.data)
+        node = node.next
+        fast = fast.next.next
+    # Skip middle element if odd number of nodes
+    if fast != None:
+        node = node.next
+    # Traverse the rest of the list checking if elements are the same
+    while node != None:
+        if node.data != stack[-1]:
+            return False
+        stack.pop()
+        node = node.next
+    return True
 
 
 if __name__ == '__main__':
-    ll = LinkedList(['t', 'a', 'c', 'c', 'a', 't'])
+    ll = LinkedList([0, 1, 2, 2, 1, 0])
     print(is_palindrome(ll.head))
+    print(is_palindrome_o(ll.head))
