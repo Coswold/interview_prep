9664254f6f5287f3ed3e2c71b01ca5d78cb4c226
Connor Oswold <coswoldo@gmail.com>
2019-05-17 16:22:18 -0700

    fixed bugs

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -1,16 +1,16 @@
 """Q2.7"""
-from linked_list import LinkedList
+from linked_list import LinkedList, Node
 
 def intersection(ll1, ll2):
-    current1 = ll1.head
+    current1 = ll1
     length1 = 0
-    while current1 != None:
+    while current1.next != None:
         length1 += 1
         current1 = current1.next
 
-    current2 = ll2.head
+    current2 = ll2
     length2 = 0
-    while current2 != None:
+    while current2.next != None:
         length2 += 1
         current2 = current2.next
 
@@ -19,23 +19,40 @@ def intersection(ll1, ll2):
 
     if length1 > length2:
         length = length1 - length2
-        current = ll1.head
+        current = ll1
         while length > 0:
             current = current.next
             length -= 1
+        current2 = ll2
+        while current != current2:
+            current = current.next
+            curren2 = current2.next
         return current
 
     else:
         length = length2 - length1
-        current = ll2.head
+        current = ll2
         while length > 0:
             current = current.next
             length -= 1
+        current1 = ll1
+        while current != current1:
+            current = current.next
+            current1 = current1.next
         return current
 
 
 
 if __name__ == '__main__':
-    ll1 = LinkedList([0, 1, 2, 2, 1, 0])
-    ll2 = LinkedList([2, 2, 1, 0])
-    print(intersection(ll1, ll2))
+    # 1 -> 2 -> 3 -> 8
+    # 1 -> 4 -> 8 -> 16 ^ 3
+    node1 = Node(1)
+    node1.next = Node(2)
+    node1.next.next = Node(3)
+    node1.next.next.next = Node(8)
+    node2 = Node(1)
+    node2.next = Node(4)
+    node2.next.next = Node(8)
+    node2.next.next = Node(16)
+    node2.next.next.next = node1.next.next
+    print(intersection(node1, node2))
