aee2d10809854be8c116647aee785b863fa5162e
Connor Oswold <coswoldo@gmail.com>
2019-05-18 17:14:53 -0700

    still broken

--- linked_lists/ll_buffer.py
+++ linked_lists/ll_buffer.py
@@ -2,15 +2,16 @@ from linked_list import LinkedList
 
 class Circular_Buffer(object):
 
-    def __init__(self, data):
+    def __init__(self, data=None):
         self.buffer = LinkedList()
         self.size = 0
         for i in range(0, 8):
             self.buffer.append(None)
         self.buffer.tail.next = self.buffer.head
 
-        for i in data:
-            self.insert(i)
+        if data != None:
+            for i in data:
+                self.insert(i)
 
     def __str__(self):
         """Return a formatted string representation of this linked list."""
@@ -20,11 +21,9 @@ class Circular_Buffer(object):
     def items(self):
         items = []
         current = self.buffer.head
-        i = 0
-        while i < self.size:
+        while current.next != self.buffer.tail:
             items.append(current.data)
             current = current.next
-            i += 1
         return items
 
     def insert(self, data):
@@ -36,7 +35,16 @@ class Circular_Buffer(object):
         if self.size < 8:
             self.size += 1
 
+    def delete(self):
+        temp = self.buffer.tail.data
+        self.buffer.tail.data = None
+        self.buffer.tail = self.buffer.tail.next
+        self.size -= 1
+        return temp
+
 
 if __name__ == '__main__':
-    ll = Circular_Buffer([0, 1, 2, 2, 1, 0, 7, 8, 9, 10, 12])
+    ll = Circular_Buffer()
+    ll.insert(8)
+    ll.insert("hello")
     print(ll)
