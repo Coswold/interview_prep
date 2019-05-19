e488a2ae48da668c01c8b950c8ede5163b7367af
Connor Oswold <coswoldo@gmail.com>
2019-05-19 14:51:23 -0700

    working on better solution

--- linked_lists/loop_detection.py
+++ linked_lists/loop_detection.py
@@ -1,6 +1,7 @@
 """Q2.8"""
 from linked_list import Node
 
+"""First approach"""
 def loop_detection(node):
     visited = {}
     while node != None:
@@ -11,13 +12,26 @@ def loop_detection(node):
         node = node.next
     return None
 
+"""Optimized solution (broken)"""
+def loop_detection2(node):
+    node = node.next
+    fast = node.next.next
+    while fast != None:
+        print(node, fast)
+        if fast == node:
+            return node
+        fast = fast.next.next
+        node = node.next
+
+    return None
+
 if __name__ == '__main__':
-    # 1 -> 2 -> 3 -> 8
-    # 1 -> 4 -> 8 -> 16 ^ 2
     node1 = Node('A')
     node1.next = Node('B')
     node1.next.next = Node('C')
     node1.next.next.next = Node('D')
-    node1.next.next.next.next = node1.next.next
+    node1.next.next.next.next = Node('E')
+    node1.next.next.next.next.next = node1.next
 
-    print(loop_detection(node1))
+    #print(loop_detection(node1))
+    print(loop_detection2(node1))
