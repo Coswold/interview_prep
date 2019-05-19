8356eb6be6a57b609f49221870e1cd9ecd0e138a
Connor Oswold <coswoldo@gmail.com>
2019-05-19 14:59:36 -0700

    completed better solution

--- linked_lists/loop_detection.py
+++ linked_lists/loop_detection.py
@@ -14,16 +14,23 @@ def loop_detection(node):
 
 """Optimized solution (broken)"""
 def loop_detection2(node):
-    node = node.next
-    fast = node.next.next
-    while fast != None:
-        print(node, fast)
-        if fast == node:
-            return node
+    slow = node
+    fast = node
+    while fast != None and fast.next != None:
+        slow = slow.next
         fast = fast.next.next
-        node = node.next
+        if fast == slow:
+            break
 
-    return None
+    if fast == None or fast.next == None:
+        return None
+
+    slow = node
+    while slow != fast:
+        slow = slow.next
+        fast = fast.next
+
+    return fast
 
 if __name__ == '__main__':
     node1 = Node('A')
@@ -33,5 +40,5 @@ if __name__ == '__main__':
     node1.next.next.next.next = Node('E')
     node1.next.next.next.next.next = node1.next
 
-    #print(loop_detection(node1))
+    print(loop_detection(node1))
     print(loop_detection2(node1))
