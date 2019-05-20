be5f4a0a0513038da56cb14f22c8ed28ab245550
Connor Oswold <coswoldo@gmail.com>
2019-05-19 21:40:29 -0700

    building out multistack

---
+++ stacks_&_queues/three_in_one.py
@@ -0,0 +1,15 @@
+"""Q3.1"""
+
+class MultiStack(object):
+
+    def __init__(self, size=1), count=3:
+        self.stack_capacity = size
+        self.stack_count = count
+        self.stack = [None] * size * count
+        self.stack1 = 0
+        self.stack2 = len(self.stack) // count
+        self.stack3 = (2 * len(self.stack)) // count
+
+
+if __name__ == '__main__':
+    three_in_one(stack)
