eccdb8c1a7ebddf30e556dd60df75d40be78117e
Connor Oswold <coswoldo@gmail.com>
2019-05-20 18:19:52 -0700

    working multi stack

--- stacks_&_queues/three_in_one.py
+++ stacks_&_queues/three_in_one.py
@@ -18,6 +18,8 @@ class MultiStack(object):
     def __index_of_top__(self, stack_num):
         offset = stack_num * self.stack_capacity
         size = self.sizes[stack_num]
+        if size == 0:
+            size = 1
         return offset + size - 1
 
     def push(self, stack_num, value):
@@ -26,6 +28,16 @@ class MultiStack(object):
         else:
             self.sizes[stack_num] += 1
             self.values[self.__index_of_top__(stack_num)] = value
+
+    def pop(self, stack_num):
+        if self.is_empty(stack_num) == True:
+            raise ValueError('Stack is empty')
+        else:
+            top = self.__index_of_top__(stack_num)
+            value = self.values[top]
+            self.values[top] = None
+            self.sizes[stack_num] -= 1
+            return value
 
     def peek(self, stack_num):
         return self.values[self.__index_of_top__(stack_num)]
@@ -37,13 +49,11 @@ class MultiStack(object):
         return self.sizes[stack_num] == 0
 
 
-
-
 if __name__ == '__main__':
     stack = MultiStack(2, 4)
     stack.push(0, 'hello')
+    stack.push(1, 'there')
+    stack.push(3, 'wow')
     print(stack)
-    stack.push(2, 'there')
-    stack.push(2, 'wow')
-    print(stack)
+    print(stack.pop(1))
     print(stack.values)
