584b488a5e375c5ad82471c68345ff6f31869514
Connor Oswold <coswoldo@gmail.com>
2019-06-05 16:21:02 -0700

    broken implementation

--- graphs_&_trees/balanced.py
+++ graphs_&_trees/balanced.py
@@ -1,7 +1,20 @@
 """Q4.4"""
 
-def balanced(root):
+from binary_tree import BinarySearchTree
+
+def balanced(root, left, right):
+    if root.left != None:
+        left += 1
+        balanced(root.left, left, right)
+    if root.right != None:
+        right += 1
+        balanced(root.right, left, right)
+    else:
+        return right == left
+
 
 if __name__ == '__main__':
-    tree = BinarySearchTree([5, 6, 3, 8, 4, 10, 1, 2])
-    print(balanced())
+    tree = BinarySearchTree([1, 2, 0.5, 3, 4, 5, 6])
+    left = 0
+    right = 0
+    print(balanced(tree.root, left, right))
