1cf90d9c26b22e994964ab5413d511642a6f454d
Coswold <coswoldo@gmail.com>
2019-06-29 19:28:38 -0700

    in order successor

---
+++ graphs_&_trees/successor.py
@@ -0,0 +1,36 @@
+class BinaryNodeParent(object):
+
+    def __init__(self, parent=None, data=None):
+        self.data = data
+        self.parent = parent
+        self.left = None
+        self.right = None
+
+
+def successor(node):
+    if node == None:
+        return None
+    if node.right != None:
+        return leftMost(node.right)
+    else:
+        q = node
+        x = q.parent
+        while x != None and x.left != q:
+            q = x
+            x = x.parent
+        return x.data
+
+def leftMost(node):
+    if node == None:
+        return None
+    while node.left != None:
+        node = node.left
+    return node
+
+
+if __name__ == '__main__':
+    n1 = BinaryNodeParent()
+    n1.data = 8
+    n1.left = BinaryNodeParent(n1, 5)
+    n1.right = BinaryNodeParent(n1, 10)
+    print(successor(n1.left))
